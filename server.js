ff72ea5ee025c7d51f73b415ce1739219872207b
Cybrix <cybrixtheproot@proton.me>
2025-11-07 00:28:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const path = require('path');
 
 const assets = express()
 
-assets.use((req, res, next)=>{
-    console.log(req.url);
-    next()
-})
+
 
 
 /*
@@ -16,6 +13,17 @@ assets.use((req, res, next)=>{
 assets.get('/assets/23e7ad7cb0a5a4cf75e07c9e0848b1eb06bba15e8fa9b8cb0579fc823c532927', (req, res) => {
   res.sendFile(path.join(__dirname, 'static_assets', 'Aero.ttf'));
 });
+
+
+assets.get('/assets/415dc6290378574135b64c808dc640c1df7531973290c4970c51fdeb849cb0c5', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static_assets', 'Aero.ttf'));
+});
+
+
+assets.use((req, res, next)=>{
+    console.log("UNKNOW ASSET! " + req.url);
+    next()
+})
 
 
 assets.listen(7777,()=>{
