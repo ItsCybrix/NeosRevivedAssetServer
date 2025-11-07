f9128d51fc856b2194dca6accd2b73d2ba1224b4
Cybrix <cybrixtheproot@proton.me>
2025-11-07 00:33:34 -0500

    Just a lot of BS testing

--- server.js
+++ server.js
@@ -11,13 +11,10 @@ const assets = express()
 */
 
 assets.get('/assets/23e7ad7cb0a5a4cf75e07c9e0848b1eb06bba15e8fa9b8cb0579fc823c532927', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static_assets', 'Aero.ttf'));
+  res.sendFile(path.join(__dirname, 'static_assets', 'NotoSansMath-Regular.ttf'));
 });
 
 
-assets.get('/assets/415dc6290378574135b64c808dc640c1df7531973290c4970c51fdeb849cb0c5', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static_assets', 'Aero.ttf'));
-});
 
 
 assets.use((req, res, next)=>{
