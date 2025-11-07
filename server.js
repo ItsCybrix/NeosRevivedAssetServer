f712ba3b01b66f0ba51aebd587df9a5928549aa9
Cybrix <cybrixtheproot@proton.me>
2025-11-07 00:19:49 -0500

    Init font

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const path = require('path');
 
+const assets = express()
 
-const api = express()
-
-api.use((req, res, next)=>{
+assets.use((req, res, next)=>{
     console.log(req.url);
-    res.send("OK")
+    next()
 })
 
 
-api.listen(7777,()=>{
+/*
+* Static Assets
+*/
+
+assets.get('/assets/23e7ad7cb0a5a4cf75e07c9e0848b1eb06bba15e8fa9b8cb0579fc823c532927', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static_assets', 'Aero.ttf'));
+});
+
+
+assets.listen(7777,()=>{
     console.log("Asset service running...")
 })
